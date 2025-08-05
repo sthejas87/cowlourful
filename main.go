263f4a042839c10b808cb78b80a4c331f542f392
sthejas87 <89645460+sthejas87@users.noreply.github.com>
2025-08-05 16:55:22 +0530

    feat: implement lolcat-style text colouring

--- main.go
+++ main.go
@@ -5,7 +5,22 @@ import (
 	"os"
 	"io"
 	"fmt"
+	"math"
 )
+
+func getColour (index, total int) string {
+	
+	//change spread and intensity of colours
+	freq := 0.3
+	phase := 4.0
+
+	//calculate RGB values by shifting sine waves from [-1,1] to [0,255] for full colour range
+	r := math.Sin(freq*float64(index)+phase)*127.5 + 127.5
+	g := math.Sin(freq*float64(index)+phase+2)*127.5 + 127.5
+	b := math.Sin(freq*float64(index)+phase+4)*127.5 + 127.5
+
+	return fmt.Sprintf("\x1b[38;2;%d;%d;%dm", int(r), int(g), int(b))
+}
 
 func main(){
 	
@@ -15,7 +30,9 @@ func main(){
 		fmt.Println("The command is intended to work with pipes")
 		os.Exit(1)
 	}
-
+	
+	RESET := "\x1b[0m"
+	
 	var output[] rune
 
 	reader := bufio.NewReader(os.Stdin) // to read input from Stdin
@@ -27,5 +44,12 @@ func main(){
 		output = append(output, input)
 	}
 
-	fmt.Println(string(output))
+	totalLength := len(output)
+	
+	for i, char := range output{
+		colour := getColour(i, totalLength)
+		fmt.Printf("%s%c", colour, char)
+	}	
+
+	fmt.Print(RESET)
 }
